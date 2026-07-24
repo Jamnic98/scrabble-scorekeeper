ea2b71460b50c970799edf2852cd545e2caaf4fd
Jamie Stimpson <jamiestimpson30@gmail.com>
2026-07-24 16:27:18 +0100

    Fix tile style

--- src/components/tile/tile.tsx
+++ src/components/tile/tile.tsx
@@ -12,9 +12,9 @@ export interface TileProps {
 type TileStyle = 'mono' | 'serif' | 'sans'
 
 const fontStyles: Record<TileStyle, string> = {
-  mono: 'font-mono font-bold',
-  serif: 'font-serif font-bold',
-  sans: 'font-sans font-bold'
+  mono: 'font-mono font-bold left-[8px] top-[18px]',
+  sans: 'font-sans font-bold left-[6px] top-[15px]',
+  serif: 'font-serif font-bold left-[6px] top-[15px]'
 }
 
 const baseStyle =
@@ -24,19 +24,19 @@ export const Tile: React.FC<TileProps> = ({
   letter = '',
   isBlank = false,
   unavailable = false,
-  style = 'sans'
+  style = 'mono'
 }) => (
   <div
     className={`${baseStyle} ${unavailable ? 'bg-[rgb(255,218,163)]/30 border-black/40' : 'bg-[rgb(255,218,163)] border-black'}`}
     data-testid="application-tile"
   >
-    <div
-      className={`${fontStyles[style]} text-center tracking-normal ${unavailable ? 'text-black/40' : 'text-black'}`}
-    >
+    <div className={`text-center tracking-normal ${unavailable ? 'text-black/40' : 'text-black'}`}>
       {/* display letter */}
-      <span className="text-2xl absolute left-1.5 top-4 leading-0">{letter.toUpperCase()}</span>
+      <span className={`${fontStyles[style]} text-2xl absolute leading-0`}>
+        {letter.toUpperCase()}
+      </span>
       {/* display digit */}
-      <span className="absolute text-[9px] bottom-1 right-px leading-0 font-sans">
+      <span className="absolute text-[9px] bottom-1 right-px leading-0 font-sans font-bold">
         {isBlank ? null : getLetterValue(letter)}
       </span>
     </div>
